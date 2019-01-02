263b9a8c518d8df8e4893889f22db2adabfd4f1c
Jonathan Tang <tangjon@hotmail.com>
2019-01-01 19:29:55 -0800

    test another format

--- frontend/src/components/About/AboutDetail.js
+++ frontend/src/components/About/AboutDetail.js
@@ -1,14 +1,12 @@
 import React from 'react'
 
-class AboutDetail extends React.Component {
-    render() {
-        return (
-            <div className="card-chip">
-                <img src={this.props.image} className="card-image" alt=""></img>
-                <div className="card-text">{this.props.title}</div>
-            </div>
-        )
-    }
+const AboutDetail = function (props) {
+    return (
+        <div className="card-chip">
+            <img src={props.image} className="card-image" alt=""></img>
+            <div className="card-text">{props.title}</div>
+        </div>
+    )
 }
 
 export default AboutDetail
